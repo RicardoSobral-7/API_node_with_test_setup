f2460ffe58366d84dcf56251a96b2b9909c5aa5a
Ricardo Sobral <riki.sobral@gmail.com>
2024-02-19 19:42:56 -0300

    feat: init config

---
+++ src/server.js
@@ -0,0 +1,11 @@
+import http from 'node:http';
+import { json } from './middlewares/json.js';
+
+const server = http.createServer(async (req, res) => {
+
+  await json(req, res);
+
+  return res.writeHead(404).end()
+})
+
+server.listen(3333)
