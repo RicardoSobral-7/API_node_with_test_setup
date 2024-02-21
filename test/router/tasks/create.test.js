fbbfb39c296cbc79074a65bd44923505233aba78
Ricardo Sobral <riki.sobral@gmail.com>
2024-02-21 11:07:38 -0300

    feat: initial test

---
+++ test/router/tasks/create.test.js
@@ -0,0 +1,47 @@
+import { describe, it } from 'node:test'
+import assert from 'node:assert'
+
+describe('Create tasks', () => {
+  it('Should not create task witout title', async () => {
+    const data = {
+      description: "teste descrição"
+    }
+    const response = await fetch('http://localhost:3333/tasks', {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json"
+      },
+      body: JSON.stringify(data)
+    })
+    assert.deepEqual(response.status, 400)
+  })
+
+  it('Should not create task witout description', async () => {
+    const data = {
+      description: "teste descrição"
+    }
+    const response = await fetch('http://localhost:3333/tasks', {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json"
+      },
+      body: JSON.stringify(data)
+    })
+    assert.deepEqual(response.status, 400)
+  })
+
+  it('Should create task', async () => {
+    const data = {
+      title: "Teste de criação",
+      description: "teste descrição"
+    }
+    const response = await fetch('http://localhost:3333/tasks', {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json"
+      },
+      body: JSON.stringify(data)
+    })
+    assert.deepEqual(response.status, 201)
+  })
+})
